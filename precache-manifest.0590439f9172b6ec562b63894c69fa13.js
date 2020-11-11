5662994e3652b63a6745cee25b409ffd4743fdd5
Felipe Salles Lopes <felipe28101997@gmail.com>
2020-11-11 11:41:43 -0400

    Updates

---
+++ precache-manifest.0590439f9172b6ec562b63894c69fa13.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd5b1d2764478b2ee981992d22e82171",
+    "url": "/react-weather-api/index.html"
+  },
+  {
+    "revision": "be76d3b2e1d90674748c",
+    "url": "/react-weather-api/static/js/2.a89daece.chunk.js"
+  },
+  {
+    "revision": "e33bd41e3cbb3743b58dfbc09e761595",
+    "url": "/react-weather-api/static/js/2.a89daece.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75b96c797fe93af31dea",
+    "url": "/react-weather-api/static/js/main.c1438681.chunk.js"
+  },
+  {
+    "revision": "80adb42bfea7d3d611bd",
+    "url": "/react-weather-api/static/js/runtime-main.31f94cbe.js"
+  },
+  {
+    "revision": "a022c03fc14eb5e42c07cbdc4343bc82",
+    "url": "/react-weather-api/static/media/01d.a022c03f.png"
+  },
+  {
+    "revision": "3e01708e7aa4b2946fa2adbe0cfa99f9",
+    "url": "/react-weather-api/static/media/01n.3e01708e.png"
+  },
+  {
+    "revision": "dc242b0f930b70f2a22bba4e1306b0d6",
+    "url": "/react-weather-api/static/media/02d.dc242b0f.png"
+  },
+  {
+    "revision": "b298dd7864d1ef41388bc24937b064ef",
+    "url": "/react-weather-api/static/media/02n.b298dd78.png"
+  },
+  {
+    "revision": "78f898e56f9a0175463bed76293f414a",
+    "url": "/react-weather-api/static/media/03d.78f898e5.png"
+  },
+  {
+    "revision": "feadd39eb6241b08f5fc3ca4cfc42906",
+    "url": "/react-weather-api/static/media/03n.feadd39e.png"
+  },
+  {
+    "revision": "1014fdb7398e3677770f93b3563a6213",
+    "url": "/react-weather-api/static/media/04d.1014fdb7.png"
+  },
+  {
+    "revision": "f70df8fc2a60aa6ee28a20b6dcc460f0",
+    "url": "/react-weather-api/static/media/04n.f70df8fc.png"
+  },
+  {
+    "revision": "0d004c061f1e5ee711e5083c85fcc0f0",
+    "url": "/react-weather-api/static/media/09d.0d004c06.png"
+  },
+  {
+    "revision": "0d004c061f1e5ee711e5083c85fcc0f0",
+    "url": "/react-weather-api/static/media/09n.0d004c06.png"
+  },
+  {
+    "revision": "4b1a85390c292be18e3c1756497f2cd5",
+    "url": "/react-weather-api/static/media/10d.4b1a8539.png"
+  },
+  {
+    "revision": "7241b374cd143d154305bf2d4d92c461",
+    "url": "/react-weather-api/static/media/10n.7241b374.png"
+  },
+  {
+    "revision": "245e2485ae6e7c1921545d3570375c41",
+    "url": "/react-weather-api/static/media/11d.245e2485.png"
+  },
+  {
+    "revision": "245e2485ae6e7c1921545d3570375c41",
+    "url": "/react-weather-api/static/media/11n.245e2485.png"
+  },
+  {
+    "revision": "be802cec7e991c0660ff7a1d4dff4486",
+    "url": "/react-weather-api/static/media/13d.be802cec.png"
+  },
+  {
+    "revision": "be802cec7e991c0660ff7a1d4dff4486",
+    "url": "/react-weather-api/static/media/13n.be802cec.png"
+  },
+  {
+    "revision": "216e9d889c3ff5bbb15b6013604d16de",
+    "url": "/react-weather-api/static/media/mqbackground.216e9d88.jpg"
+  },
+  {
+    "revision": "2bbb238e08065b1858f5ce24686bd213",
+    "url": "/react-weather-api/static/media/no-location.2bbb238e.png"
+  }
+]);
